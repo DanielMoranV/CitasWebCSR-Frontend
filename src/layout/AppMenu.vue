ac6d9e6c25756c792cf5514211bcfc3688e8f67c
Daniel Moran <skaan.dmv@gmail.com>
2023-09-15 17:24:40 -0500

    Lista de medicos

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -2,6 +2,9 @@
 import { ref, onMounted, watch } from 'vue';
 import AppMenuItem from './AppMenuItem.vue';
 import { useAuthStore } from '../stores/auth';
+import { useDataDoctorStore } from '../stores/dataDoctor';
+
+const dataDoctorStore = useDataDoctorStore();
 
 const authStore = useAuthStore();
 
@@ -12,6 +15,7 @@ const defaultMenuModel = [
             { label: 'Registarse', icon: 'pi pi-fw pi-user', to: '/signin', roles: ['Invitado'] },
             { label: 'Perfil', icon: 'pi pi-fw pi-user', to: '/profile', roles: ['Administrador', 'Admisionista', 'Médico', 'Paciente'] },
             { label: 'Dependientes', icon: 'pi pi-fw pi-users', to: '/dependents', roles: ['Paciente'] },
+            { label: 'Médicos', icon: 'pi pi-fw pi-plus', to: '/listdoctor', roles: ['Paciente'] },
             { label: 'Agendar Cita', icon: 'pi pi-fw pi-calendar-plus', to: '/quotes', roles: ['Paciente'] },
             { label: 'Seguimiento', icon: 'pi pi-fw pi-chart-line', to: '/tracking', roles: ['Paciente'] }
         ]
@@ -36,16 +40,36 @@ const defaultMenuModel = [
 const model = ref([]);
 
 function updateMenuModel() {
+    // Obtiene el rol del usuario actual desde el objeto authStore
     const userRole = authStore.sessionUser ? authStore.role : 'Invitado';
 
-    model.value = defaultMenuModel
-        .map((section) => ({
-            ...section,
-            items: section.items.filter((item) => item.roles.includes(userRole))
-        }))
-        .filter((section) => section.items.length > 0);
-}
+    // Verifica si existe un objeto doctor en dataDoctorStore
+    const hasDoctor = dataDoctorStore.doctor !== null && typeof dataDoctorStore.doctor === 'object';
 
+    // Filtra y modifica el modelo de menú en función del rol del usuario y la existencia de un doctor
+    model.value = defaultMenuModel
+        .map((section) => {
+            if (section.label === 'Citas Medicas' && userRole === 'Paciente' && !hasDoctor) {
+                // Si el usuario es 'Paciente' y no existe un doctor, excluye el elemento "Agendar Cita"
+                return {
+                    ...section,
+                    items: section.items.filter((item) => item.roles.includes(userRole) && item.label !== 'Agendar Cita')
+                };
+            } else {
+                // En otros casos, simplemente filtra según el rol del usuario
+                return {
+                    ...section,
+                    items: section.items.filter((item) => item.roles.includes(userRole))
+                };
+            }
+        })
+        .filter((section) => section.items.length > 0);
+
+    // Si existe un objeto doctor en dataDoctorStore, se muestra un mensaje en la consola
+    if (hasDoctor) {
+        console.log(dataDoctorStore.doctor);
+    }
+}
 onMounted(async () => {
     await authStore.currentUser();
     updateMenuModel();
