085f54b85743520cc2c697315f3280075fc11c8e
Daniel Moran Vilchez <skaan.dmv@gmail.com>
2023-12-01 10:07:26 -0500

    go

--- src/views/admission/Patients.vue
+++ src/views/admission/Patients.vue
@@ -10,7 +10,8 @@ const toast = useToast();
 const dataUserStore = useDataUserStore();
 
 const users = ref(null);
-const user = ref({});
+const listPatients = ref([]);
+const patient = ref({});
 const userDialog = ref(false);
 const deleteUserDialog = ref(false);
 const deleteUsersDialog = ref(false);
@@ -29,23 +30,19 @@ const civilStatusItems = ref([
     { name: 'Divorciado', code: 'Divorciado' }
 ]);
 
-const roleNames = ref({
-    1: 'Administrador',
-    2: 'Admisionista'
-});
 const isValidPhone = (value) => {
     const phonePattern = /^[0-9]{9}$/; // Este patrón asume un número de 9 dígitos.
 
     return phonePattern.test(value);
 };
 const isValidDni = (value) => {
-    if (user.value.user.documentType === 'DNI') {
+    if (patient.value.documentType === 'DNI') {
         // Para DNI, verificar que solo contiene 8 dígitos de 0-9
         return /^\d{8}$/.test(value);
-    } else if (user.value.user.documentType === 'CE') {
+    } else if (patient.value.documentType === 'CE') {
         // Para Carnet de extranjería, verificar que solo contiene 9 dígitos de 0-9
         return /^\d{9}$/.test(value);
-    } else if (user.value.user.documentType === 'Pasaporte') {
+    } else if (patient.value.documentType === 'Pasaporte') {
         // Para Pasaporte, verificar que contiene letras y números y tiene hasta 20 caracteres
         return /^[A-Za-z0-9]{5,20}$/.test(value);
     }
@@ -55,27 +52,28 @@ const isValidDni = (value) => {
 onBeforeMount(() => {
     initFilters();
 });
-const filename = `listcolaborator-${dformat(new Date(), 'DD-MM-YYYY')}`;
+const filename = `listpatients-${dformat(new Date(), 'DD-MM-YYYY')}`;
 onMounted(async () => {
     await dataUserStore.getPatients().then((data) => {
+        console.log(data);
         users.value = data.flatMap((user) => {
             // Crear un array que contiene tanto el usuario como sus dependientes
             const combinedArray = [{ user: user.user }, ...(user.user.dependents || []).map((dependent) => ({ user: dependent }))];
-            console.log(combinedArray);
             return combinedArray;
         });
     });
 
-    users.value.map((user) => {
-        let birthDate = dformat(user.user.birthDate, 'DD MMMM YYYY');
-        user.user.birthDate = birthDate;
-        user = user.user;
+    users.value.forEach((user) => {
+        const currentUser = user.user;
+        let birthDate = dformat(currentUser.birthDate, 'DD MMMM YYYY');
+        currentUser.birthDate = birthDate;
+        listPatients.value.push(currentUser);
     });
-    console.log('holi', users.value);
+    console.log(listPatients.value);
 });
 
 const openNew = () => {
-    user.value = {
+    patient.value = {
         accessId: null,
         active: null,
         createAt: null,
@@ -109,53 +107,51 @@ const hideDialog = () => {
 };
 
 const validateRequiredFields = () => {
-    const userValue = user.value.user;
+    const userValue = patient.value;
     return userValue.name && userValue.name.trim() && userValue.surnames && userValue.dni && userValue.birthDate && userValue.sex && isValidDni(userValue.dni) && isValidPhone(userValue.phone);
 };
 
 const updateUser = async () => {
     const payload = {
-        address: user.value.user.address,
-        birthDate: dparse(new Date(user.value.user.birthDate)),
-        civilStatus: user.value.user.civilStatus,
-        dni: user.value.user.dni,
-        documentType: user.value.user.documentType,
-        email: user.value.user.email,
-        name: user.value.user.name,
-        phone: user.value.user.phone,
-        photo: user.value.user.photo,
-        sex: user.value.user.sex,
-        surnames: user.value.user.surnames,
+        address: patient.value.address,
+        birthDate: dparse(new Date(patient.value.birthDate)),
+        civilStatus: patient.value.civilStatus,
+        dni: patient.value.dni,
+        documentType: patient.value.documentType,
+        email: patient.value.email,
+        name: patient.value.name,
+        phone: patient.value.phone,
+        photo: patient.value.photo,
+        sex: patient.value.sex,
+        surnames: patient.value.surnames,
         access: {
-            username: `${user.value.user.dni}`,
-            password: user.value.user.dni,
+            username: `${patient.value.dni}`,
+            password: patient.value.dni,
             roleId: 4
         }
     };
 
-    if (user.value.accessId) {
-        const userIndex = users.value.findIndex((item) => item.accessId === user.value.accessId);
+    if (patient.value.accessId) {
+        const userIndex = listPatients.value.findIndex((item) => item.accessId === patient.value.accessId);
         if (userIndex !== -1) {
-            payload.access.accessId = user.value.accessId;
-            payload.userId = user.value.user.userId;
+            payload.access.accessId = patient.value.accessId;
+            payload.userId = patient.value.userId;
 
             await dataUserStore.updateUser(payload.dni, payload.access.accessId, payload);
 
-            user.value.roleName = roleNames.value[user.value.roleId];
-            users.value[userIndex] = user.value;
+            listPatients.value[userIndex] = patient.value;
             toast.add({ severity: 'success', summary: 'Éxito', detail: 'Colaborador Actualizado', life: 3000 });
         }
     } else {
         const dataUser = await dataUserStore.addUsers(payload);
-        user.value.accessId = dataUser.access[0].accessId;
-        user.value.roleName = roleNames.value[user.value.roleId];
-        user.value.status = 'offline';
-        users.value.push(user.value);
+        patient.value.accessId = dataUser.access[0].accessId;
+        patient.value.status = 'offline';
+        listPatients.value.push(patient.value);
         toast.add({ severity: 'success', summary: 'Éxito', detail: 'Colaborador Registrado', life: 3000 });
     }
 
     userDialog.value = false;
-    user.value = {};
+    patient.value = {};
     submitted.value = false;
 };
 
@@ -168,21 +164,21 @@ const saveUser = async () => {
 };
 
 const editUser = (editUser) => {
-    user.value = { ...editUser };
+    patient.value = { ...editUser };
     userDialog.value = true;
 };
 
 const confirmDeleteUser = (editUser) => {
-    user.value = editUser;
+    patient.value = editUser;
     deleteUserDialog.value = true;
 };
 
 const deleteUser = async () => {
-    console.log(user.value.user);
-    user.value = users.value.filter((val) => val.user.accessId !== user.value.user.accessId);
-    await dataUserStore.disableUser(user.value.accessId);
+    console.log(patient.value);
+    patient.value = listPatients.value.filter((val) => val.user.accessId !== patient.value.accessId);
+    await dataUserStore.disableUser(patient.value.accessId);
     deleteUserDialog.value = false;
-    user.value = {};
+    patient.value = {};
     toast.add({ severity: 'success', summary: 'Successful', detail: 'User Deleted', life: 3000 });
 };
 
@@ -215,8 +211,8 @@ const confirmDeleteSelected = () => {
     deleteUsersDialog.value = true;
 };
 const deleteSelectedUsers = () => {
-    users.value = users.value.filter((val) => !selectedUsers.value.includes(val));
-    const selectedIds = selectedUsers.value.map((user) => user.accessId);
+    listPatients.value = listPatients.value.filter((val) => !selectedUsers.value.includes(val));
+    const selectedIds = selectedUsers.value.map((user) => user.dni);
 
     selectedIds.forEach(async (accessId) => {
         await dataUserStore.disableUser(accessId);
@@ -263,9 +259,9 @@ const initFilters = () => {
 
                 <DataTable
                     ref="dt"
-                    :value="users"
+                    :value="listPatients"
                     v-model:selection="selectedUsers"
-                    dataKey="accessId"
+                    dataKey="dni"
                     :paginator="true"
                     :rows="10"
                     :filters="filters"
@@ -289,31 +285,31 @@ const initFilters = () => {
                     <Column field="user.dni" header="DNI" :sortable="true" headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">DNI</span>
-                            {{ slotProps.data.user.dni }}
+                            {{ slotProps.data.dni }}
                         </template>
                     </Column>
                     <Column field="user.name" header="Nombre" :sortable="true" headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">Nombre</span>
-                            {{ slotProps.data.user.name }}
+                            {{ slotProps.data.name }}
                         </template>
                     </Column>
                     <Column field="user.surnames" header="Apellidos" :sortable="true" headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">Apellidos</span>
-                            {{ slotProps.data.user.surnames }}
+                            {{ slotProps.data.surnames }}
                         </template>
                     </Column>
                     <Column field="roleName" header="Fecha de Nacimiento" :sortable="true" headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">Fecha Nacimiento</span>
-                            {{ slotProps.data.user.birthDate }}
+                            {{ slotProps.data.birthDate }}
                         </template>
                     </Column>
                     <Column field="status" header="Teléfono" :sortable="true" headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">Teléfono</span>
-                            {{ slotProps.data.user.phone }}
+                            {{ slotProps.data.phone }}
                         </template>
                     </Column>
                     <Column headerStyle="min-width:10rem;" header="Acciones">
@@ -330,55 +326,55 @@ const initFilters = () => {
                         <label class="mb-3">Tipo de documento</label>
                         <div class="formgrid grid">
                             <div class="field-radiobutton col-4">
-                                <RadioButton id="dni" name="option" value="DNI" v-model="user.user.documentType" />
+                                <RadioButton id="dni" name="option" value="DNI" v-model="patient.documentType" />
                                 <label for="dni">DNI</label>
                             </div>
                             <div class="field-radiobutton col-4">
-                                <RadioButton id="ce" name="option" value="CE" v-model="user.user.documentType" />
+                                <RadioButton id="ce" name="option" value="CE" v-model="patient.documentType" />
                                 <label for="ce">CE</label>
                             </div>
                             <div class="field-radiobutton col-4">
-                                <RadioButton id="pasaport" name="option" value="Pasaporte" v-model="user.user.documentType" />
+                                <RadioButton id="pasaport" name="option" value="Pasaporte" v-model="patient.documentType" />
                                 <label for="pasaport">Pasaporte</label>
                             </div>
                         </div>
                     </div>
                     <div class="field">
-                        <label for="dni">{{ user.user.documentType }}</label>
-                        <InputText id="name" v-model.trim="user.user.dni" required="true" autofocus :class="{ 'p-invalid': (submitted && !user.user.dni) || !isValidDni(user.user.dni) }" />
-                        <small class="p-invalid" v-if="(submitted && !user.user.dni) || !isValidDni(user.user.dni)">{{ user.user.documentType }} es requerido o formato inválido</small>
+                        <label for="dni">{{ patient.documentType }}</label>
+                        <InputText id="name" v-model.trim="patient.dni" required="true" autofocus :class="{ 'p-invalid': (submitted && !patient.dni) || !isValidDni(patient.dni) }" />
+                        <small class="p-invalid" v-if="(submitted && !patient.dni) || !isValidDni(patient.dni)">{{ patient.documentType }} es requerido o formato inválido</small>
                     </div>
                     <div class="field">
                         <label for="name">Nombre</label>
-                        <InputText id="name" v-model.trim="user.user.name" required="true" autofocus :class="{ 'p-invalid': submitted && !user.user.name }" />
+                        <InputText id="name" v-model.trim="patient.name" required="true" autofocus :class="{ 'p-invalid': submitted && !patient.name }" />
                         <small class="p-invalid" v-if="submitted && !user.name">Nombre es requerido.</small>
                     </div>
 
                     <div class="field">
                         <label for="name">Apellidos</label>
-                        <InputText id="name" v-model.trim="user.user.surnames" required="true" autofocus :class="{ 'p-invalid': submitted && !user.user.surnames }" />
-                        <small class="p-invalid" v-if="submitted && !user.user.surnames">Apellido es requerido.</small>
+                        <InputText id="name" v-model.trim="patient.surnames" required="true" autofocus :class="{ 'p-invalid': submitted && !patient.surnames }" />
+                        <small class="p-invalid" v-if="submitted && !patient.surnames">Apellido es requerido.</small>
                     </div>
                     <div class="formgrid grid">
                         <div class="field col">
                             <label for="birthDate">Fecha de Nacimiento</label>
-                            <Calendar :showIcon="true" :showButtonBar="true" v-model="user.user.birthDate" dateFormat="dd/mm/yy" required="true"></Calendar>
+                            <Calendar :showIcon="true" :showButtonBar="true" v-model="patient.birthDate" dateFormat="dd/mm/yy" required="true"></Calendar>
                             <small class="p-invalid" v-if="submitted && !user.birthDate">Fecha de nacimiento es requerido.</small>
                         </div>
                         <div class="field col">
                             <label for="phone">Teléfono</label>
-                            <InputText id="phone" v-model.trim="user.user.phone" :class="{ 'p-invalid': (submitted && !user.user.phone) || !isValidPhone(user.user.phone) }" />
-                            <small class="p-invalid" v-if="(submitted && !user.user.phone) || !isValidPhone(user.user.phone)"> Teléfono es requerido o formato inválido</small>
+                            <InputText id="phone" v-model.trim="patient.phone" :class="{ 'p-invalid': (submitted && !patient.phone) || !isValidPhone(patient.phone) }" />
+                            <small class="p-invalid" v-if="(submitted && !patient.phone) || !isValidPhone(patient.phone)"> Teléfono es requerido o formato inválido</small>
                         </div>
                     </div>
                     <div class="formgrid grid">
                         <div class="field col">
                             <label for="sex">Sexo</label>
-                            <Dropdown id="sex" v-model="user.user.sex" :options="sexItems" optionLabel="name" placeholder="Selecciona" optionValue="code"></Dropdown>
+                            <Dropdown id="sex" v-model="patient.sex" :options="sexItems" optionLabel="name" placeholder="Selecciona" optionValue="code"></Dropdown>
                         </div>
                         <div class="field col">
                             <label for="role">Estado Civil</label>
-                            <Dropdown id="civilStatus" v-model="user.user.civilStatus" :options="civilStatusItems" optionLabel="name" placeholder="Selecciona" optionValue="code"></Dropdown>
+                            <Dropdown id="civilStatus" v-model="patient.civilStatus" :options="civilStatusItems" optionLabel="name" placeholder="Selecciona" optionValue="code"></Dropdown>
                         </div>
                     </div>
                     <template #footer>
@@ -391,7 +387,7 @@ const initFilters = () => {
                     <div class="flex align-items-center justify-content-center">
                         <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
                         <span v-if="user"
-                            >¿Estás seguro de que quieres eliminar <b>{{ user.user.name }}</b
+                            >¿Estás seguro de que quieres eliminar <b>{{ patient.name }}</b
                             >?</span
                         >
                     </div>
