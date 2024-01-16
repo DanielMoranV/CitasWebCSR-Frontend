66e44b06d26fcf5d494f443c68dbc6d36495114d
Daniel Moran Vilchez <skaan.dmv@gmail.com>
2024-01-16 17:36:44 -0500

    Https

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const backendURL = 'http://localhost:8080';
+//export const backendURL = 'http://localhost:8080';
 //export const backendURL = 'https://api.csr.net.pe';
+export const backendURL = 'https://csr-backend-v3jh.onrender.com';
